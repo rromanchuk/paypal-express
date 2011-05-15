18e1994762c4a435bc7ec06133e3b20ea337b4c9
Ryan Romanchuk <rromanchuk@gmail.com>
2011-05-15 16:20:04 -0700

    quick and dirty add to support :items => [] for detailed item listing on paypal express checkout

--- lib/paypal/express/response.rb
+++ lib/paypal/express/response.rb
@@ -2,10 +2,17 @@ module Paypal
   module Express
     class Response < NVP::Response
       attr_accessor :pay_on_paypal
-
+      attr_accessor :detailed_items
+      
       def initialize(response, options = {})
         super response
         @pay_on_paypal = options[:pay_on_paypal]
+        @detailed_items = {} 
+        options[:items].is_a?(Array) && options[:items].each_with_index do |entry, index|
+          @detailed_items["L_PAYMENTREQUEST_0_NAME#{index}"]    = entry[:name]
+          @detailed_items["L_PAYMENTREQUEST_0_NUMBER#{index}"]  = entry[:qty]
+          @detailed_items["L_PAYMENTREQUEST_0_DESC#{index}"]    = entry[:desc]
+        end
       end
 
       def redirect_uri
@@ -26,6 +33,7 @@ module Paypal
         _query_ = {:token => self.token}
         _query_.merge!(:cmd => '_express-checkout') if with_cmd
         _query_.merge!(:useraction => 'commit')     if pay_on_paypal
+        _query_.merge!(detailed_items)              unless detailed_items.blank?
         _query_
       end
     end
